c7d7792c9c4f11ca599581da4442cf447499f8c6
Bill Foote <billf@jovial.com>
2024-05-08 11:13:30 -0700

    Update build:  APK now done via github

--- make_android.sh
+++ make_android.sh
@@ -3,26 +3,26 @@ cd `dirname $0`
 rm -rf tmp.android
 cd jrpn15
 flutter clean
-flutter build apk --release
-if [ ! -e build/app/outputs/flutter-apk/app-release.apk ] ; then
-    echo "Oops!"
-    exit 1
-fi
+# flutter build apk --release
+# if [ ! -e build/app/outputs/flutter-apk/app-release.apk ] ; then
+#     echo "Oops!"
+#     exit 1
+# fi
 flutter build appbundle --release
-if [ ! -e build/app/outputs/flutter-apk/app-release.apk ] ; then
+if [ ! -e build/app/outputs/bundle/release/app-release.aab ] ; then
     echo "Oops!"
     exit 1
 fi
 cd ..
 cd jrpn16
 flutter clean
-flutter build apk --release
-if [ ! -e build/app/outputs/flutter-apk/app-release.apk ] ; then
-    echo "Oops!"
-    exit 1
-fi
+# flutter build apk --release
+# if [ ! -e build/app/outputs/flutter-apk/app-release.apk ] ; then
+#     echo "Oops!"
+#     exit 1
+# fi
 flutter build appbundle --release
-if [ ! -e build/app/outputs/flutter-apk/app-release.apk ] ; then
+if [ ! -e build/app/outputs/bundle/release/app-release.aab ] ; then
     echo "Oops!"
     exit 1
 fi
@@ -30,5 +30,5 @@ cd ..
 
 mkdir tmp.android
 echo "made directory tmp.android"
-mv jrpn16/build/app/outputs/flutter-apk/app-release.apk tmp.android/jrpn16_android_app-release.apk
-mv jrpn15/build/app/outputs/flutter-apk/app-release.apk tmp.android/jrpn15_android_app-release.apk
+mv jrpn16/build/app/outputs/bundle/release/app-release.aab tmp.android/jrpn16_app-release.aab
+mv jrpn15/build/app/outputs/bundle/release/app-release.aab tmp.android/jrpn15_app-release.aab
