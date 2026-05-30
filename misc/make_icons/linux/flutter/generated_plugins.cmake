acea8f0d2257ae9f07d120091d094640fc7a3b03
Bill Foote <billf@jovial.com>
2026-05-30 21:31:12 +0200

    Versioning

--- misc/make_icons/linux/flutter/generated_plugins.cmake
+++ misc/make_icons/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  desktop_multi_window
   file_selector_linux
   gtk
   url_launcher_linux
--- misc/make_icons/macos/Flutter/GeneratedPluginRegistrant.swift
+++ misc/make_icons/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import app_links
-import desktop_multi_window
 import file_selector_macos
 import path_provider_foundation
 import share_plus
@@ -15,7 +14,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  FlutterMultiWindowPlugin.register(with: registry.registrar(forPlugin: "FlutterMultiWindowPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
